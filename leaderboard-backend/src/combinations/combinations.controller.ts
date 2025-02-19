8f230728a3cda1585fd43b3070f12ae1388abaef
Markus Wagner <MWagner@pcs.com>
2025-02-19 08:03:36 +0100

    Add OpenAPI Generation to LeaderBoard Service

--- leaderboard-backend/src/combinations/combinations.controller.ts
+++ leaderboard-backend/src/combinations/combinations.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { CombinationsService } from './combinations.service';
 import { CombinationsScore } from './model/combinations.model';
+import { ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('combinations')
 export class CombinationsController {
   constructor(private combinationsService: CombinationsService) {}
 
   @Get('')
+  @ApiOkResponse({
+    description: "Combinations used to win the game and their score",
+    type: CombinationsScore,
+    isArray: true
+  })
   public async getTopCombinations(): Promise<CombinationsScore[]> {
     return await this.combinationsService.getTopCombinations();
   }
--- leaderboard-backend/src/combinations/model/combinations.model.ts
+++ leaderboard-backend/src/combinations/model/combinations.model.ts
@@ -1,6 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CombinationsScore {
-  constructor(
-    public board: string[],
-    public score: number,
-  ) {}
+    @ApiProperty({
+        minLength: 9,
+        maxLength: 9,
+        isArray: true,
+        type: 'string',
+        description: 'Board setup for a winning combination',
+    })
+    public board: string[];
+
+    @ApiProperty({
+        description: 'Number of times someone won with this combination',
+    })
+    public score: number;
+
+    constructor(board: string[], score: number) {
+        this.board = board;
+        this.score = score;
+    }
 }
