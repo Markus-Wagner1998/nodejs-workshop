811fb18d42cae82a5a4621d103ea0333b1698875
Markus Wagner <markus.wagner1998@gmail.com>
2025-02-27 17:58:01 +0100

    Removed everything from frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { PlayComponent } from './play/play.component';
 
 export const routes: Routes = [
-  { path: '', component: LeaderboardComponent },
-  { path: 'tictactoe', component: PlayComponent },
+  { path: '', component: PlayComponent },
 ];
