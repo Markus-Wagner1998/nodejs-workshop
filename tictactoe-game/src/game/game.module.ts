e77a3213f4103818453d823f03fbb21a1e59a042
Markus Wagner <markus.wagner1998@gmail.com>
2025-02-27 21:07:46 +0100

    Removed non-imported module

--- tictactoe-game/src/game/game.module.ts
+++ tictactoe-game/src/game/game.module.ts
@@ -3,31 +3,14 @@ import { GameController } from './game.controller';
 import { GameService } from './game.service';
 import { GameUtilService } from 'src/game/game-util/game-util.service';
 import { OpponentService } from 'src/game/opponent/opponent.service';
-import { SQS } from '@aws-sdk/client-sqs';
 
 @Module({
   controllers: [GameController],
   providers: [
     GameService,
     GameUtilService,
-    OpponentService,
-    {
-      provide: SQS,
-      useFactory: () => {
-        const endpoint = 'http://localhost:4566';
-        const region = 'eu-central-1';
-        const accessKeyId = 'ACCESS_KEY';
-        const secretAccessKey = 'SECRET_ACCESS_KEY';
-        const sqsConfig = {
-          endpoint,
-          region,
-          accessKeyId,
-          secretAccessKey,
-        };
-        return new SQS(sqsConfig);
-      },
-    },
+    OpponentService
   ],
-  exports: [SQS],
+  exports: [],
 })
 export class GameModule {}
