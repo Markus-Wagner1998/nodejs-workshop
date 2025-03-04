6bba8a36bdd103a058e6a3dbca34643edd4b16ae
Markus Wagner <markus.wagner1998@gmail.com>
2025-03-04 08:05:27 +0100

    Made Service Tests runnable

--- tictactoe-game/src/game/game.controller.spec.ts
+++ tictactoe-game/src/game/game.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameController } from './game.controller';
+import { GameService } from './game.service';
+import { GameModule } from './game.module';
+import { SqsModule } from '@ssut/nestjs-sqs';
+import { GameUtilService } from './game-util/game-util.service';
+import { OpponentService } from './opponent/opponent.service';
 
 describe('GameController', () => {
   let controller: GameController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        GameModule,
+        SqsModule.register({
+          consumers: [],
+          producers: [],
+        }),
+      ],
       controllers: [GameController],
+      providers: [GameService, GameUtilService, OpponentService],
     }).compile();
 
     controller = module.get<GameController>(GameController);
--- tictactoe-game/src/game/opponent/opponent.service.spec.ts
+++ tictactoe-game/src/game/opponent/opponent.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OpponentService } from './opponent.service';
+import { GameUtilService } from '../game-util/game-util.service';
 
 describe('OpponentService', () => {
   let service: OpponentService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OpponentService],
+      providers: [OpponentService, GameUtilService],
     }).compile();
 
     service = module.get<OpponentService>(OpponentService);
